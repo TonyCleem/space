9a120fd993663c8108309fff288cc264a1ac335b
TonyCleem <tonycleem@gmail.com>
2024-10-24 20:16:04 +0700

    download photos from NASA

--- tg.py
+++ tg.py
@@ -8,18 +8,6 @@ from dotenv import load_dotenv
 from urllib.parse import unquote, urlsplit
 
 
-
-def get_extension(url):
-    parse = urlsplit(url)
-    path = parse.path
-    image_name = os.path.split(path)
-    path, name = image_name
-    decode = unquote(name)
-    result = os.path.splitext(name)
-    file, extension = result
-    return extension
-
-
 def get_image_from_nasa(url, api_key):
     directory = Path(r'C:\Devman\iso_tg\image')
     directory.mkdir(parents=True, exist_ok=True)
