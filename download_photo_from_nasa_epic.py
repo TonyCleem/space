a47607643e9148c15ff8aabbcc320ef33cce3adc
TonyCleem <tonycleem@gmail.com>
2024-11-01 22:54:07 +0700

    remove unlikvid func

--- download_photo_from_nasa_epic.py
+++ download_photo_from_nasa_epic.py
@@ -19,24 +19,15 @@ def create_parser():
     return parser
 
 
-def formats_data(date):
-    adatetime = datetime.datetime.fromisoformat(date)
-    formatted_date_1 = adatetime.strftime("%Y/%m/%d")
-    return formatted_date_1
-
-
-def generates_url_nasa_epic(date, image_name):
-    generated_url = f"https://epic.gsfc.nasa.gov/archive/natural/{date}/png/{image_name}"
-    return generated_url
-
-
-def creates_image_links(epic_data):
+def creates_valid_image_links(epic_data):
     for date_and_file in epic_data:
         date = date_and_file['date']
         filename = date_and_file['image']
         image_name = f'{filename}.png'
-        date = formats_data(date)
-        image_links.append(generates_url_nasa_epic(date, image_name))
+        adatetime = datetime.datetime.fromisoformat(date)
+        date = adatetime.strftime("%Y/%m/%d")
+        generated_url = f"https://epic.gsfc.nasa.gov/archive/natural/{date}/png/{image_name}"
+        image_links.append(generated_url)
 
 
 if __name__ == '__main__':
@@ -49,7 +40,7 @@ if __name__ == '__main__':
     image_links = []
     url = f'https://epic.gsfc.nasa.gov/api/natural/date/{date}'
     epic_data = get_json_data_from_api(url)
-    creates_image_links(epic_data)
+    creates_valid_image_links(epic_data)
     downloads_images_from_api(image_links, path)
 
     if not date:
