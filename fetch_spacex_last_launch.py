529a26359cb296ac2519a96354a185d945f173b8
TonyCleem <tonycleem@gmail.com>
2024-11-03 14:52:38 +0700

    Update fetch_spacex_last_launch.py

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -20,8 +20,8 @@ def create_parser():
 def get_images_links_from_last_launch(url):
     response = requests.get(url)
     response.raise_for_status
-    api_spacex_data = response.json()
-    images_links = api_spacex_data['links']['flickr']['original']
+    launch_details = response.json()
+    images_links = launch_details['links']['flickr']['original']
     return images_links
 
 
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     else:
         path = Path('./image/')
         path.mkdir(parents=True, exist_ok=True)
-        api_spacex_data = get_json_data_from_api(url)
-        images_links = api_spacex_data['links']['flickr']['original']
+        launch_details = get_json_data_from_api(url)
+        images_links = launch_details['links']['flickr']['original']
         downloads_images_from_images_links(images_links, path)
         print('Загружены все фотографии с указанного ID запуска')
