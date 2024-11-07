295a38347a680d8c75c71460e77605f8ba386504
TonyCleem <tonycleem@gmail.com>
2024-11-07 22:35:21 +0700

    fix nested send_images_to_tg_channel_with_timer

--- send_images_to_tg_channel_with_timer.py
+++ send_images_to_tg_channel_with_timer.py
@@ -33,25 +33,37 @@ if __name__ == '__main__':
 
     parser = create_parser()
     args = parser.parse_args()
-    hours = args.time * 3600
+    hours = args.time #* 3600
 
-    if get_images_of_directory(path):
-        for image in images:
-            file_path = path / image
-            send_image_with_tg_bot(bot, tg_channel_id, file_path)
-            print(f'Изображение {image} отправлено в телеграм канал {tg_channel_id}')
-            time.sleep(hours)
-        print('Все файлы из каталога отправлены. Начинается отправка случайных изображений из каталога')
-        while True:
-            images = get_images_of_directory(path)
-            random.shuffle(images)
-            image = images[0]
-            file_path = path / image
-            send_image_with_tg_bot(bot, tg_channel_id, file_path)
-            print(f'Изображение {image} отправлено в телеграм канал {tg_channel_id}')
-            time.sleep(hours)
-    else:
-        print('Каталог пуст')
+    images =  get_images_of_directory(path)
+    for image in images:
+        file_path = path / image
+        send_image_with_tg_bot(bot, tg_channel_id, file_path)
+        print(f'Изображение {image} отправлено в телеграм канал {tg_channel_id}')
+        time.sleep(hours)
+    print('Все файлы из каталога отправлены. Начинается отправка случайных изображений из каталога')
+    while True:
+        images = get_images_of_directory(path)
+        if not images:
+            print('Каталог пуст')
+            break
+        random.shuffle(images)
+        image = images[0]
+        file_path = path / image
+        send_image_with_tg_bot(bot, tg_channel_id, file_path)
+        print(f'Изображение {image} отправлено в телеграм канал {tg_channel_id}')
+        time.sleep(hours)
+
+
+
+
+
+
+
+
+
+
+   
         
 
     
